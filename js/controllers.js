9ddd64dca335d2dc1e872f06bac2b288fd16937e
Tessa <tessathornton@gmail.com>
2014-06-25 16:08:02 -0400

    options page that works

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,16 @@ angular.module('scrumDont.controllers', []).
 
   controller('ScrumCtrl', function ($scope, Project) {
 
-    $scope.member = localStorage.member;
+    $scope.member = localStorage.member !== undefined ? JSON.parse(localStorage.member) : '';
 
-    $scope.projectSlug = localStorage.projectSlug;
+    $scope.currentProject = localStorage.currentProject !== undefined ? JSON.parse(localStorage.currentProject) : '';
+
+    $scope.saveOptions = function(currentProject, member) {
+      if (member && currentProject) {
+        localStorage['member'] = JSON.stringify(member);
+        localStorage['currentProject'] = JSON.stringify(currentProject);
+      }
+    }
     
     Project.query(function(data){
       $scope.projects = data;  
