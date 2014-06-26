5c8a9ce6b4728c8c1be66190e8686fcd3f6e188d
Tessa <tessathornton@gmail.com>
2014-06-26 11:02:51 -0400

    loading indicator

--- js/app.js
+++ js/app.js
@@ -1,7 +1,40 @@
-angular.module('scrumDont', [
+var scrumDont = angular.module('scrumDont', [
   'ngResource',
   'scrumDont.services',
   'scrumDont.controllers'
   ]);
 
+scrumDont.config(function($httpProvider) {
 
+  $httpProvider.interceptors.push(function($q, $rootScope) {
+    return {
+      'request': function(config) {
+        $rootScope.$broadcast('loading-started');
+        return config || $q.when(config);
+      },
+      'response': function(response) {
+        $rootScope.$broadcast('loading-complete');
+        return response || $q.when(response);
+      }
+    };
+  });
+
+});
+
+
+scrumDont.directive("loadingIndicator", function() {
+  return {
+    restrict : "A",
+    template: "<div></div>",
+    link : function(scope, element, attrs) {
+      scope.$on("loading-started", function(e) {
+        element.css({"display" : ""});
+      });
+
+      scope.$on("loading-complete", function(e) {
+        element.css({"display" : "none"});
+      });
+
+    }
+  };
+});
